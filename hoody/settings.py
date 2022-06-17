13352f6eab3e5074e739b20c044b40cf5a9e1809
Michael Ogaye <hogaeymic@gmail.com>
2022-06-17 22:25:24 +0300

    logout and login urls redirects

--- hoody/settings.py
+++ hoody/settings.py
@@ -181,3 +181,6 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL='/'
+LOGIN_REDIRECT_URL='/'
+
