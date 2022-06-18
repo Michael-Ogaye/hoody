2f903cd3fa60ded40d0713cadd741f9a0421eff9
Michael Ogaye <hogaeymic@gmail.com>
2022-06-18 12:50:55 +0300

    static files configurations

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -33,6 +34,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=254, blank=True)
     profile_picture = CloudinaryField('images')
+    
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
